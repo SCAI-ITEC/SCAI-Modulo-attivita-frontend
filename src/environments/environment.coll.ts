b30be6d72a3f95d8279c83fe1cb029be584455d5
angelicazenere <angelica.zenere@scaiitec.it>
2023-03-22 15:46:22 +0100

    agigunta be modulo attivita per collaudo

--- src/environments/environment.coll.ts
+++ src/environments/environment.coll.ts
@@ -2,5 +2,5 @@ import { ENV_COLL } from "./envs";
 
 export const environment = {
     name: ENV_COLL,
-    statoAvanzamentoUrl: ""
+    statoAvanzamentoUrl: "https://modulo-attivita-be-collaudo-dot-scai-intranet-gruppo-coll.ew.r.appspot.com"
 };
