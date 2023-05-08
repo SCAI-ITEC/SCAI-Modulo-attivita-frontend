e76d5235dfef9f80a473fca06132d3af56bbc5eb
angelicazenere <angelica.zenere@scaiitec.it>
2023-05-08 21:45:09 +0200

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'rootatt',
     loadChildren: () =>
       import('./dashboard/dashboard.module')
         .then(m => m.DashboardModule),
